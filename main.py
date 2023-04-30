8eb7b3d564ca9b18268b87c6e53a1d8eba7674b6
zheedong <zheedong@gmail.com>
2023-04-30 16:34:06 +0900

    PDF summarise and write in GPT

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import json
 import openai
+import fitz
+from time import time
 
-open_ai_api_key = json.load(open(".open_ai_api.json"))["key"]
-print(open_ai_api_key)
-openai.api_key = open_ai_api_key
+openai.api_key = json.load(open(".openai_api_key.json"))["key"]
 
-response = openai.Completion.create(
-    engine="davinci",
-    prompt="This is a test",
-    temperature=0.3,
-    max_tokens=60,
-    top_p=1.0,
-    frequency_penalty=0.0,
-    presence_penalty=0.0,
-    stop=["\n"]
-)
+PDF_FILE_PATH = "./data/sample1.pdf"
+doc = fitz.open(PDF_FILE_PATH)
+for page in doc:
+    text = page.get_text()
+    print(text)
 
-print(response)
-print(response.choices[0].text)
+f = open(f"summary/test_{time()}.md", "w")
+
+for page in doc:
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt="Summarise following text and give some additional examples and translate it into Korean in markdown format:\n" + page.get_text(),
+        temperature=0.3,
+        max_tokens=60,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n"]
+    )
+    f.write(response.choices[0].text)
+
+f.close()
